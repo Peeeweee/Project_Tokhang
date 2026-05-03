ed7699dc08785c20c54b41c77c2527532d10fd41
Peeeweee <kentpaulodelgado@gmail.com>
2026-05-03 18:57:49 +0800

    build: configure GitHub Pages deployment and project metadata

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tokhang-presentation/",
+  base: "/Project_Tokhang/",
 })
